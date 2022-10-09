cf9fce3027e33e60c5f7f0900385f61cdbf2c551
philippe.bernet <philippe.bernet@epita.fr>
2022-10-09 22:37:32 +0200

    wip: cpu: pipeline

--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,36 @@
 #include <iostream>
 #include <png++/png.hpp>
 
+
+
 void pipeline(png::image<png::rgb_pixel> ref, png::image<png::rgb_pixel> modified)
 {
     (void) ref;
     (void) modified;
+
+    // 1.Greyscale
+    // 2.Smooth (gaussian filter)
+    // 3.Difference
+    // 4.Closing/opening with disk or rectangle
+    // 5.1.Thresh image
+    // 5.2.Lakes
+    // 6.Output Json
+    /*
+    Json format example
+    {
+        "input-0001.jpg" : [
+            [0, 0, 10, 10],
+            [15, 15, 42, 42]
+        ],
+        "input-0002.jpg" : [],
+        "input-0003.jpg" : [
+            [0, 0, 10, 10],
+            [15, 15, 42, 42],
+            [51, 42, 69, 99]
+        ]
+    }
+    */
+
 }
 
 int main(int argc, char *argv[])
@@ -16,6 +42,9 @@ int main(int argc, char *argv[])
     }
 
     png::image<png::rgb_pixel> ref(argv[1]);
+
+    auto buf = ref.get_pixbuf();
+    std::cout << buf[0][0].blue << std::endl;
 
     for (int i = 2; i < argc; i++)
     {
