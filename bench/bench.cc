feed230c9039e3281b5134d59b6b446f8658202a
Bernet <philippe.bernet@epita.fr>
2022-11-01 17:09:18 +0100

    feat(bench): scia premium

--- bench/bench.cc
+++ bench/bench.cc
@@ -3,7 +3,7 @@
 #include <png++/png.hpp>
 #include "pipeline.hh"
 
-void BM_Detection_cpu(benchmark::State &st, std::string ref_filename, std::string modified_filename)
+void BM_Detection_cpu(benchmark::State &st, std::string ref_filename, std::string modified_filename, enum mode_cc mode_cc)
 {
     png::image<png::rgb_pixel> ref(ref_filename);
     png::image<png::rgb_pixel> modified(modified_filename);
@@ -17,7 +17,6 @@ void BM_Detection_cpu(benchmark::State &st, std::string ref_filename, std::strin
     int kernel_size_closing = 41;
     int binary_threshold = 10;
     int minimum_pixel = 30;
-    enum mode_cc mode_cc = slide;
 
     auto ref_greyscale = cpu::greyscale(ref.get_pixbuf(), width, height);
     auto ref_smoothed = cpu::smoothing(ref_greyscale, width, height, kernel_size);
@@ -28,8 +27,34 @@ void BM_Detection_cpu(benchmark::State &st, std::string ref_filename, std::strin
     std::free(ref_smoothed);
 }
 
-BENCHMARK_CAPTURE(BM_Detection_cpu, nuit_blanche_02, std::string("../data/01.png"), std::string("../data/02.png"));
-BENCHMARK_CAPTURE(BM_Detection_cpu, nuit_blanche_03, std::string("../data/01.png"), std::string("../data/03.png"));
-BENCHMARK_CAPTURE(BM_Detection_cpu, nuit_blanche_04, std::string("../data/01.png"), std::string("../data/04.png"));
+void BM_Detection_gpu(benchmark::State &st, std::string ref_filename, std::string modified_filename, enum mode_cc mode_cc)
+{
+    png::image<png::rgb_pixel> ref(ref_filename);
+    png::image<png::rgb_pixel> modified(modified_filename);
+    auto pixbuf = modified.get_pixbuf();
+
+    int width = ref.get_width();
+    int height = ref.get_height();
+
+    int kernel_size = 5;
+    int kernel_size_opening = 101;
+    int kernel_size_closing = 41;
+    int binary_threshold = 20;
+    int minimum_pixel = 30;
+
+    auto ref_greyscale = cpu::greyscale(ref.get_pixbuf(), width, height);
+    auto ref_smoothed = cpu::smoothing(ref_greyscale, width, height, kernel_size);
+    auto d_ref_smoothed = gpu::malloc_and_copy(ref_smoothed, width, height);
+
+    for (auto _ : st)
+        auto components = gpu::pipeline(d_ref_smoothed, modified.get_pixbuf(), width, height, kernel_size, kernel_size_opening, kernel_size_closing, binary_threshold, mode_cc, minimum_pixel);
+
+    std::free(ref_smoothed);
+    gpu::my_cuda_free(d_ref_smoothed);
+}
+
+BENCHMARK_CAPTURE(BM_Detection_cpu, scia_premium_slide, std::string("../data/scia_premium_0001.png"), std::string("../data/scia_premium_0002.png"), slide);
+BENCHMARK_CAPTURE(BM_Detection_cpu, scia_premium_union_find, std::string("../data/scia_premium_0001.png"), std::string("../data/scia_premium_0002.png"), union_find);
+BENCHMARK_CAPTURE(BM_Detection_gpu, scia_premium_slide, std::string("../data/scia_premium_0001.png"), std::string("../data/scia_premium_0002.png"), union_find);
 
 BENCHMARK_MAIN();
